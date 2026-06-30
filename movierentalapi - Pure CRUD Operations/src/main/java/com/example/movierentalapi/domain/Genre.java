201848b8bcdf59a56fdc6c2c770e1eee8fcea75c
Mahlatse-Seloane <mahlatseseloane97@gmail.com>
2026-06-30 22:22:54 +0200

    Add files via upload
    Initial architecture scaffold

---
+++ movierentalapi - Pure CRUD Operations/src/main/java/com/example/movierentalapi/domain/Genre.java
@@ -0,0 +1,18 @@
+package com.example.movierentalapi.domain;
+
+public enum Genre
+{
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    FANTASY,
+    HORROR,
+    MYSTERY,
+    ROMANCE,
+    SCI_FI,
+    THRILLER,
+    WESTERN,
+    DOCUMENTARY,
+    MUSICAL;
+}
---
+++ movierentalapi - Pure CRUD Operations/src/test/java/com/example/movierentalapi/MovierentalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movierentalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierentalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
